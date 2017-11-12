1f1930146918b316c72f24edbf229971272334ff
Ye Jian <se7ent1ng@foxmail.com>
2017-11-12 12:33:09 +0800

    添加命令行解析功能

--- hello.py
+++ hello.py
@@ -2,8 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+from flask_script import Manager
 
 app = Flask(__name__)
+
+manager = Manager(app)
 
 
 @app.route('/')
@@ -17,4 +20,4 @@ def user(name):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
